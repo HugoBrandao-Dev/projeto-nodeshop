a2a0669d3e6ed7aabdfee7029fefb6101663866b
HugoBrandao <hugobrandao.dev@gmail.com>
2022-03-22 20:55:59 -0400

    Adicionadas as rotas e as views de edicao e informacoes detalhadas para de um produto. Modificacoes na disposicao dos elementos nas views.

--- produtos/produtosController.js
+++ produtos/produtosController.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
+let produtos = [
+	{
+		id: 0,
+		categoria: 1,
+		tipo: 1,
+		marca: 'Dell',
+		modelo: 'Dell2012',
+		fabricacao: '2020',
+		garantiaLoja: 2,
+		garantiaFabricante: 4,
+		estoque: 100,
+		preco: 5000,
+	},
+	{
+		id: 1,
+		categoria: 1,
+		tipo: 1,
+		marca: 'Apple',
+		modelo: 'Macbook Air',
+		fabricacao: '2020',
+		garantiaLoja: 4,
+		garantiaFabricante: 8,
+		estoque: 10,
+		preco: 10000,
+	}
+]
+
 router.get('/admin/produtos', (req, res) => {
-	res.render('admin/produtos/produtosLista', { admin: 1 })
+
+	res.render('admin/produtos/produtosLista', { admin: 1, produtos })
 })
 
 router.get('/admin/produto/novo', (req, res) => {
@@ -10,21 +38,65 @@ router.get('/admin/produto/novo', (req, res) => {
 })
 
 router.post('/admin/produto/salvar', (req, res) => {
-	let marca = req.body.iptMarca
-	let produto = req.body.iptProduto
-	let fabricacao = req.body.iptFabricacao
-	let garantiaLoja = req.body.iptGarantiaLoja
-	let garantiaFabricante = req.body.iptGarantiaFabricante
-	let categoria = req.body.sltCategoria
-	let estoque = req.body.iptEstoque
-	res.json({
+	let categoria = Number.parseInt(req.body.sltCategoria.value)
+	let tipo = Number.parseInt(req.body.sltTipo.value)
+	let marca = Number.parseInt(req.body.iptMarca)
+	let modelo = req.body.iptModelo
+	let fabricacao = Number.parseInt(req.body.iptFabricacao)
+	let garantiaLoja = Number.parseInt(req.body.iptGarantiaLoja)
+	let garantiaFabricante = Number.parseInt(req.body.iptGarantiaFabricante)
+	let estoque = Number.parseInt(req.body.iptEstoque)
+	let preco = Number.parseFloat(parseFloat(req.body.iptPreco.replace(',', '.')).toFixed(2))
+
+	produtos.push({
+		categoria,
+		tipo,
 		marca,
-		produto,
+		modelo,
 		fabricacao,
 		garantiaLoja,
 		garantiaFabricante,
+		estoque,
+		preco
+	})
+	res.redirect('/admin/produtos', { admin: 1 })
+})
+
+router.get('/admin/produto/:id', (req, res) => {
+	let id = req.params.id
+	let produto = produtos.filter(produto => produto.id == id)[0]
+	res.render('admin/produtos/produtoInfo', { admin: 1, produto })
+})
+
+router.get('/admin/produto/edit/:id', (req, res) => {
+	let id = req.params.id
+	let produto = produtos.filter(produto => produto.id == id)[0]
+	console.log(produto)
+	res.render('admin/produtos/produtoEditar', { admin: 1, produto })
+})
+
+router.post('/admin/produto/atualizar/:id', (req, res) => {
+	console.log(req.body.sltCategoria.value)
+	let categoria = Number.parseInt(req.body.sltCategoria.value)
+	let tipo = Number.parseInt(req.body.sltTipo.value)
+	let marca = Number.parseInt(req.body.iptMarca)
+	let modelo = req.body.iptModelo
+	let fabricacao = Number.parseInt(req.body.iptFabricacao)
+	let garantiaLoja = Number.parseInt(req.body.iptGarantiaLoja)
+	let garantiaFabricante = Number.parseInt(req.body.iptGarantiaFabricante)
+	let estoque = Number.parseInt(req.body.iptEstoque)
+	let preco = Number.parseFloat(parseFloat(req.body.iptPreco.replace(',', '.')).toFixed(2))
+
+	res.json({
 		categoria,
-		estoque
+		tipo,
+		marca,
+		modelo,
+		fabricacao,
+		garantiaLoja,
+		garantiaFabricante,
+		estoque,
+		preco
 	})
 })
 
