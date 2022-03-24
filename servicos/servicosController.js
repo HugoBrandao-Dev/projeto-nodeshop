25dd39d9e4786a6ade33b483a58f56cdff425700
HugoBrandao <hugobrandao.dev@gmail.com>
2022-03-23 23:33:44 -0400

    Criada rota e view do sistema de edicao de um servico na parte de administracao.

--- servicos/servicosController.js
+++ servicos/servicosController.js
@@ -6,22 +6,31 @@ let servicos = [
 		id: 0,
 		servico: 'Manutenção em redes de computadores',
 		responsaveis: ['Tobias de Oliveira'],
+		descricao: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime neque quis eos sint, tempore delectus tenetur pariatur? Ad similique nobis eaque quibusdam corporis architecto veritatis placeat, dolor deleniti quaerat mollitia.'
 	},
 	{
 		id: 1,
 		servico: 'Manutenção em banco de dados',
 		responsaveis: ['Tobias de Oliveira', 'Dinorá de Oliveira'],
+		descricao: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime neque quis eos sint, tempore delectus tenetur pariatur? Ad similique nobis eaque quibusdam corporis architecto veritatis placeat, dolor deleniti quaerat mollitia.'
 	},
 	{
 		id: 2,
 		servico: 'Manutenção em computadores',
-		responsaveis: ['Josias Cruz', 'doralice Cruz'],
+		responsaveis: ['Josias Cruz', 'Doralice Cruz'],
+		descricao: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime neque quis eos sint, tempore delectus tenetur pariatur? Ad similique nobis eaque quibusdam corporis architecto veritatis placeat, dolor deleniti quaerat mollitia.'
 	}
 ]
 
 // Rotas do Administrador
 router.get('/admin/servicos', (req, res) => {
 	res.render('admin/servicos/servicosLista', { admin: 1, servicos})
+})
+
+router.get('/admin/servico/edit/:id', (req, res) => {
+	let id = req.params.id
+	let servico = servicos.filter(servico => servico.id == id)[0]
+	res.render('admin/servicos/servicoEditar', { admin: 1, servico })
 })
 
 module.exports = router
