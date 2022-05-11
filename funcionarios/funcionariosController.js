7b314ee1c4d907f4d0bef16e907173d4088aaf1e
Hugo da Rocha Brandão <hugobrandao.dev@gmail.com>
2022-05-10 20:02:15 -0400

    Criado back-end para a visualizacao das informacoes de um funcionario em especifico.

--- funcionarios/funcionariosController.js
+++ funcionarios/funcionariosController.js
@@ -6,57 +6,6 @@ const Cargos = require('../cargos/CargosModel')
 const Setores = require('../setores/SetoresModel')
 const Funcionarios = require('../funcionarios/FuncionariosModel')
 
-let funcionarios = [
-	{
-		id: 0,
-		nome: 'Tobias de Oliveira',
-		cpf: '111.111.111-11',
-		nascimento: '30/01/1992',
-		endereco: 'Bairro: Oliveira I, Rua: Tobias de Oliveira, N: 100',
-		email: 'tobias@gmail.com',
-		telefone: '5511111111111',
-		celular: '5511111111112',
-		setor: 'Atendimento',
-		cargo: 'Atendente'
-	},
-	{
-		id: 1,
-		nome: 'Dinorá de Oliveira',
-		cpf: '222.222.222-2',
-		nascimento: '15/02/1993',
-		endereco: 'Bairro: Oliveira IV, Rua: Dinorá de Oliveira, N: 300',
-		email: 'dinora_oliveira@gmail.com',
-		telefone: '5522222222222',
-		celular: '5522222222223',
-		setor: 'Assistência técnica',
-		cargo: 'Técnico(a) assistente'
-	},
-	{
-		id: 2,
-		nome: 'Josias Cruz',
-		cpf: '333.333.333-3',
-		nascimento: '04/06/1995',
-		endereco: 'Bairro: Cruz III, Rua: Josias Cruz, N: 500',
-		email: 'josias_cruz@hotmail.com',
-		telefone: '5533333333333',
-		celular: '5533333333334',
-		setor: 'Assistência técnica',
-		cargo: 'Técnico(a) assistente'
-	},
-	{
-		id: 3,
-		nome: 'Doralice Cruz',
-		cpf: '444.444.444-4',
-		nascimento: '20/10/1990',
-		endereco: 'Bairro: Cruz II, Rua: Doralice Cruz, N: 1000',
-		email: 'dora_cruz@hotmail.com',
-		telefone: '5544444444444',
-		celular: '5544444444445',
-		setor: 'Gerência',
-		cargo: 'Gerente'
-	}
-]
-
 router.get('/admin/funcionarios', (req, res) => {
 	Funcionarios.findAll({
 		include: [
@@ -141,8 +90,15 @@ router.post('/admin/funcionario/deletar', (req, res) => {
 
 router.get('/admin/funcionario/:id', (req, res) => {
 	let id = req.params.id
-	let funcionario = funcionarios.filter(funcionario => funcionario.id == id)[0]
-	res.render('admin/funcionarios/funcionarioInfo', { admin: 1, funcionario })
+	Funcionarios.findByPk(id, {
+		include: [
+			{ model: Setores },
+			{ model: Cargos }
+		]
+	})
+		.then(funcionario => {
+			res.render('admin/funcionarios/funcionarioInfo', { admin: 1, funcionario })
+		})
 })
 
 router.get('/admin/funcionarios/opcoes', (req, res) => {
