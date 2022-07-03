daae76ca98ec15d2828a71f0d569256ae7510f1a
Hugo da Rocha Brandão <hugobrandao.dev@gmail.com>
2022-07-02 23:09:45 -0400

    Corrigida a relacao entre as tabelas de Compras, Produtos e Produtos vendidos.

--- produtos_vendidos/ProdutosVendidosModel.js
+++ produtos_vendidos/ProdutosVendidosModel.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize')
 const connection = require('../database/database')
 
+// Models
 const Compras = require('../compras/ComprasModel')
+const Produtos = require('../produtos/ProdutosModel')
 
 const ProdutosVendidos = connection.define('produtos_vendidos', {
 	quantidade: {
@@ -11,14 +13,15 @@ const ProdutosVendidos = connection.define('produtos_vendidos', {
 	}
 })
 
-Compras.hasMany(ProdutosVendidos)
+Compras.belongsToMany(Produtos, { through: ProdutosVendidos })
+Produtos.belongsToMany(Compras, { through: ProdutosVendidos })
 
 ProdutosVendidos.sync({ force: false })
-	.then(() => {
-		console.log('Tabela Produtos_vendidos criada com sucesso.')
-	})
-	.catch(erro => {
-		console.log('Erro na criação da tabela Produtos_vendidos. ' + erro)
-	})
+.then(() => {
+	console.log('Tabela Produtos_vendidos criada com sucesso.')
+})
+.catch(erro => {
+	console.log('Erro na criação da tabela Produtos_vendidos. ' + erro)
+})
 
 module.exports = ProdutosVendidos
